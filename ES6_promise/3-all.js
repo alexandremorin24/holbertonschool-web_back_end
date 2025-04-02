035aabcdb42895d10ae3d4f0076cb42725d50a32
alexandremorin24 <9761@holbertonstudents.com>
2025-04-02 09:35:34 +0200

    ESLint correction

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,3 +1,5 @@
+import { uploadPhoto, createUser } from './utils.js';
+
 export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
     .then(([photo, user]) => {
