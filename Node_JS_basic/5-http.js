65ba1515fe2281ae08b16891a2aec7647d22b114
alexandremorin24 <9761@holbertonstudents.com>
2025-05-23 14:13:45 +0200

    Add T5

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,58 @@
+const http = require('http');
+const fs = require('fs');
+
+// Function to read students from CSV file asynchronously
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+
+      const lines = data.split('\n').filter((line) => line.trim() !== '');
+      const students = lines.slice(1);
+
+      const fields = {};
+      for (const student of students) {
+        const [firstname, , , field] = student.split(',');
+        if (!fields[field]) {
+          fields[field] = [];
+        }
+        fields[field].push(firstname);
+      }
+
+      let output = `Number of students: ${students.length}`;
+      for (const [field, names] of Object.entries(fields)) {
+        output += `\nNumber of students in ${field}: ${names.length}. List: ${names.join(', ')}`;
+      }
+
+      resolve(output);
+    });
+  });
+}
+
+// Path to the database file from command-line argument
+const database = process.argv[2];
+
+// Create the HTTP server
+const app = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    countStudents(database)
+      .then((report) => {
+        res.end(`This is the list of our students\n${report}`);
+      })
+      .catch((err) => {
+        res.statusCode = 500;
+        res.end(err.message);
+      });
+  }
+});
+
+app.listen(1245);
+
+module.exports = app;
